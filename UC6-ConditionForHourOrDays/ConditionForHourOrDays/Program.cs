18bf1ca6bcb599d9c10f05969fbc9704b7ce5a92
prashikjaware <prashikjaware123@gmail.com>
2021-03-04 17:36:22 +0530

    [Prashik]Added: Calculating wages with the condition for specific hour and specific days

---
+++ UC6-ConditionForHourOrDays/ConditionForHourOrDays/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ConditionForHourOrDays
+{
+    class Program
+    {
+        //Constants
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+        public static void Main()
+        {
+            //variables
+            int empHrs = 0;
+            int totalWorkingDays = 0;
+            int totalEmpHrs = 0;
+            while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(0,3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day#" + totalWorkingDays + "EmpHrs:" + empHrs);
+            }
+            Console.WriteLine(totalEmpHrs+ "   "+ totalWorkingDays);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+        }
+
+    }
+}
